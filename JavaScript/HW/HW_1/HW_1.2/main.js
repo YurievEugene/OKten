925505f1d6f3e9ed39cdf9e7ed787d92e9a64dfe
YurievEugene <yurev05@gmail.com>
2026-02-16 15:26:02 +0200

    HW_1

---
+++ JavaScript/HW/HW_1/HW_1.2/main.js
@@ -0,0 +1,6 @@
+let Firstname = 'Yuriev';
+let middleName = 'Eugene';
+let lastName = 'Oleksandrovych';
+
+let person = Firstname + ' ' + middleName + ' ' + lastName;
+console.log(person)
---
+++ JavaScript/lessons/lesson_1/main.js
@@ -0,0 +1,20 @@
+{
+    let a ='Hello world';
+    var b = 'Hello me';
+}
+console.log(b);
+//console.log(a); nothing happens, just error
+
+let greeting = 'hello';
+let intro = 'my name is';
+let username = 'name';
+let msg = greeting + intro + username;
+let msg2 = `${greeting} ${intro} ${username}`;
+console.log(msg);
+console.log(msg2);
+
+
+let w = 100500; //integer
+console.log(w);
+let y = 3.1415213; //float double
+console.log(y);
